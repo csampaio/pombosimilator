8281601cfc11fdea0b034fc59d922ed56330db4c
Claudio Sampaio <claudio.sampaio@conatussw.com>
2017-01-18 22:43:11 -0400

    Commit inicial do site do pombo simulator

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1484782606,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/people-sheet0.png",
+		"images/people2-sheet0.png",
+		"images/people2-sheet1.png",
+		"images/people2-sheet2.png",
+		"images/pombo-sheet0.png",
+		"images/pombo-sheet1.png",
+		"images/pombo-sheet2.png",
+		"images/sidewalk.png",
+		"images/poop-sheet0.png",
+		"images/boundary-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
